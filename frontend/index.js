f68da0db4d421f5d9c74e47c8102a92b6b0e28b6
Rayyan <weatherwhiz06@gmail.com>
2021-02-12 15:19:44 -0500

    ready for deploy

--- frontend/index.js
+++ frontend/index.js
@@ -2,7 +2,7 @@ const BG_COLOR = '#231f20';
 const SNAKE_COLOR = "#c2c2c2";
 const FOOD_COLOR = '#e66916';
 
-const socket = io('http://localhost:3000');
+const socket = io('https://thawing-plateau-96640.herokuapp.com/');
 
 socket.on('init', handleInit);
 socket.on('gameState', handleGameState);
--- server/server.js
+++ server/server.js
@@ -105,5 +105,8 @@ function emitGameOver(roomName, winner) {
     io.sockets.in(roomName)
         .emit('gameOver', JSON.stringify({ winner }));
 }
-io.listen(3000);
+io.listen(process.env.PORT || 3000);
+
+
+
 
